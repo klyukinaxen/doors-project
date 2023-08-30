dea850f1d5f9792046be99029a3af7dac25d55ac
DaniilIsupov <disupov1996@gmail.com>
2023-08-30 21:59:22 +0200

    refactor: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
+/**
+ * @type {import('eslint/lib/shared/types').ConfigData}
+ */
 module.exports = {
     env: {
-        "browser": true,
-        "node": true
+        browser: true,
+        node: true
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/vue3-recommended',
-        'prettier',
-    ],
+    extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
     rules: {
         'vue/component-name-in-template-casing': ['error', 'PascalCase'],
         'vue/multi-word-component-names': 'off'
     }
-};
+}
