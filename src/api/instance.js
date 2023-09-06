e1dcb979a53147941fade91ea7c5648a74bcda69
DaniilIsupov <disupov1996@gmail.com>
2023-09-06 20:21:02 +0200

    feat: handle accessToken expired

--- src/api/instance.js
+++ src/api/instance.js
@@ -8,4 +8,21 @@ apiInstance.interceptors.request.use((config) => {
     return config
 })
 
+apiInstance.interceptors.response.use(
+    (response) => {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response
+    },
+    (error) => {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        if (axios.isAxiosError(error) && error.response.status === 401) {
+            localStorage.removeItem('accessToken')
+            location.reload()
+        }
+        // Do something with response error
+        return Promise.reject(error)
+    }
+)
+
 export { apiInstance }
